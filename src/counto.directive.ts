7494ccef1cc223e25c60f86c9038f073798cc80b
Igor <izupet@gmail.com>
2016-02-23 19:32:25 +0100

    Rename

--- src/counto.directive.ts
+++ src/counto.directive.ts
@@ -1,13 +1,13 @@
 import {Directive, Output, EventEmitter} from 'angular2/core';
 
 @Directive({
-    selector: 'count',
+    selector: 'counto',
     properties: ['duration: duration', 'countFrom: countFrom', 'countTo: countTo', 'step: step']
 })
 
-export class Count {
+export class Counto {
 
-    @Output() countChange = new EventEmitter();
+    @Output() countoChange = new EventEmitter();
     private _timer;
     private _duration: number;
     private _countTo: number;
@@ -37,27 +37,27 @@ export class Count {
         var _this = this;
         clearInterval(_this._timer);
 
-        if (_this._duration && _this._step && _this._countFrom && _this._countTo) {
+        if (!isNaN(_this._duration) && !isNaN(_this._step) && !isNaN(_this._countFrom) && !isNaN(_this._countTo)) {
             var intermediate  = _this._countFrom;
             var increment     = Math.abs(_this._countTo - _this._countFrom) / ((_this._duration * 1000) / _this._step);
- 
-            _this.countChange.emit(intermediate);
+
+            _this.countoChange.emit(intermediate);
 
             _this._timer = setInterval(function() {
                 if (_this._countTo < _this._countFrom) {
                     if (intermediate <= _this._countTo) {
                         clearInterval(_this._timer);
-                        _this.countChange.emit(_this._countTo);
+                        _this.countoChange.emit(_this._countTo);
                     } else {
-                        _this.countChange.emit(intermediate);
+                        _this.countoChange.emit(intermediate);
                         intermediate -= increment;
                     }
                 } else {
                     if (intermediate >= _this._countTo) {
                         clearInterval(_this._timer);
-                        _this.countChange.emit(_this._countTo);
+                        _this.countoChange.emit(_this._countTo);
                     } else {
-                        _this.countChange.emit(intermediate);
+                        _this.countoChange.emit(intermediate);
                         intermediate += increment;
                     }
                 }
