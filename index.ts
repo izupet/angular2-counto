1f7b27eabb7f76d2e1d0f989a4e3f3cb711447d2
Igor <izupet@gmail.com>
2017-04-12 23:09:03 +0200

    Fix angular-cli support

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { CountoDirective } from './src/counto.directive';
+import { CountoModule } from './src/counto.module';
+
+export { CountoDirective, CountoModule };
+export default CountoModule;
